1913efd44a3311efd951526e50e82bb66550e916
Churchill Daniel <churchillrimamskep@gmail.com>
2025-03-26 14:13:49 +0100

    Implement user creation endpoint and refactor route setup

--- main.go
+++ main.go
@@ -4,18 +4,27 @@ import (
 	"log"
 
 	"github.com/Chu-rill/Restful-go/database"
+	"github.com/Chu-rill/Restful-go/routes"
 	"github.com/gofiber/fiber/v2"
 )
 
-func main(){
-	database.ConnectDb()
-	app := fiber.New()
-
+func setupRoutes(app *fiber.App){
 	app.Get("/api",welcome)
-
-	log.Fatal(app.Listen(":3000"))
+	//User endpoints
+	app.Post("/api/users",routes.CreateUser)
 }
 
 func welcome(c *fiber.Ctx)error{
 	return c.SendString("Welcom to my awesome api")
 }
+
+
+func main(){
+	database.ConnectDb()
+	app := fiber.New()
+
+	setupRoutes(app)
+
+	log.Fatal(app.Listen(":3000"))
+}
+
--- routes/user.go
+++ routes/user.go
@@ -1,5 +1,10 @@
 package routes
 
+import (
+	"github.com/Chu-rill/Restful-go/database"
+	"github.com/Chu-rill/Restful-go/models"
+	"github.com/gofiber/fiber/v2"
+)
 
 
 type User struct{
@@ -7,4 +12,24 @@ type User struct{
 	ID uint `json:"id" gorm:"primaryKey"`
 	FirstName string `json:"first_name"`
 	LastName string `json:"last_name"`
+}
+
+func createResponseUser(userModel models.User)User{
+return User{ID: userModel.ID,FirstName: userModel.FirstName,LastName: userModel.LastName}
+}
+
+func CreateUser(c *fiber.Ctx)error{
+	var user models.User
+	
+	if err := c.BodyParser(&user);err != nil{
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error":err.Error()})
+	}
+
+	database.Databse.Db.Create(&user)
+	responseUser := createResponseUser(user)
+	return c.Status(fiber.StatusCreated).JSON(responseUser)
+}
+
+func GetUsers(c *fiber.Ctx)error{
+
 }
